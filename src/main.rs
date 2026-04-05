736095cae4212e614316bc606c7afd1bb813217b
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-04-05 10:14:05 +0100

    Convert add to post and finished up remaining errors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use axum::{Json, Router,extract::{Path }, http::StatusCode, response::IntoResponse, routing::{get,post}};
 use serde_json::{json,Value};
 use sqlx::{ Executor, sqlite::{SqliteConnectOptions, SqlitePool}};
@@ -18,7 +17,7 @@ struct DareInfo{
 #[derive(Deserialize,sqlx::FromRow)]
 struct DareRetrived{
     dare:String,
-    #[sqlx(rename="createBy")]
+    #[sqlx(rename="createdBy")]
     created_by:String
 }
 impl IntoResponse for APIError{
@@ -36,9 +35,8 @@ impl IntoResponse for APIError{
     }
 }
 async fn health_check() -> impl IntoResponse{
-    Json(json!({"status": "ok", "message": "Server up!",}))
+    Json(json!({"status": "ok", "message": "Server up",}))
 }
-
 async fn add_dare(Json(payload): Json<DareInfo>) -> Result<Json<Value>,APIError>{
     let option = SqliteConnectOptions::new().filename("dares.db").create_if_missing(true);
     let connection = SqlitePool::connect_with(option).await.unwrap();
@@ -49,8 +47,6 @@ async fn add_dare(Json(payload): Json<DareInfo>) -> Result<Json<Value>,APIError>
             createdBy Text
         )
     ").await.unwrap();
-    // let dare = params.dare;
-    // let name = params.created_by;
     sqlx::query("INSERT INTO dares (dare,createdBy) VALUES (?1,?2)").bind(&payload.dare).bind(&payload.created_by).execute(&connection).await.map_err(|_| APIError::InternalError)?;
     Ok(Json(json!({"status": "ok", "message": "dare added!",})))
 }
@@ -71,7 +67,7 @@ async fn find_dare(Path(id): Path<i64>) -> Result<Json<Value>,APIError>{
         None => Err(APIError::NotFound),
     }
 }
-async fn random_dare(Path(id): Path<i64>) -> Result<Json<Value>,APIError>{
+async fn random_dare() -> Result<Json<Value>,APIError>{
 
     let option = SqliteConnectOptions::new().filename("dares.db").create_if_missing(true);
     let connection = SqlitePool::connect_with(option).await.unwrap();
@@ -82,9 +78,9 @@ async fn random_dare(Path(id): Path<i64>) -> Result<Json<Value>,APIError>{
             createdBy Text
         )
     ").await.unwrap();
-    let row = sqlx::query_as::<_,DareRetrived>("SELECT * FROM dares ORDER BY RANDOM() LIMIT 1").bind(id).fetch_optional(&connection).await.map_err(|_| APIError::InternalError)?;
+    let row = sqlx::query_as::<_,DareRetrived>("SELECT * FROM dares ORDER BY RANDOM() LIMIT 1").fetch_optional(&connection).await.map_err(|_| APIError::InternalError)?;
     match row {
-        Some(r) =>  Ok( Json( json!({"id": id,"dare": r.dare, "created_by": r.created_by}) )),
+        Some(r) =>  Ok( Json( json!({"dare": r.dare, "created_by": r.created_by}) )),
         None => Err(APIError::NotFound),
     }
 }
@@ -93,15 +89,7 @@ fn create_app() -> Router{
 }
 #[tokio::main]
 async fn main() {
-    // let option = SqliteConnectOptions::new().filename("dares.db").create_if_missing(true);
-    // let connection = SqlitePool::connect_with(option).await.unwrap();
-    // connection.execute("
-    //     CREATE TABLE IF NOT EXISTS dares (
-    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    //         dare TEXT,
-    //         createdBy Text
-    //     )
-    // ").await.unwrap();
+
     let app = create_app();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.expect("TCP Listener not bound");
     println!("Server live on port 3000");
